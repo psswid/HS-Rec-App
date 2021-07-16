8ba0e624f22026c6b195275d5e1f586c9e38b929
Piotr Świderski <swiderski@mda.pl>
2021-07-16 12:19:58 +0200

    additional vue components

---
+++ resources/js/Components/FilterDropdown.vue
@@ -0,0 +1,62 @@
+<template>
+  <button type="button" @click="show = true">
+    <slot />
+    <div v-if="show" to="dropdown">
+      <div>
+        <div ref="dropdown" @click.stop="show = autoClose ? false : true">
+          <slot name="dropdown" />
+        </div>
+      </div>
+    </div>
+  </button>
+</template>
+
+<script>
+import Popper from 'popper.js'
+
+export default {
+  props: {
+
+    placement: {
+      type: String,
+      default: 'bottom-end',
+    },
+    boundary: {
+      type: String,
+      default: 'scrollParent',
+    },
+    autoClose: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      show: false,
+    }
+  },
+  watch: {
+    show(show) {
+      if (show) {
+        this.$nextTick(() => {
+          this.popper = new Popper(this.$el, this.$refs.dropdown, {
+            placement: this.placement,
+            modifiers: {
+              preventOverflow: { boundariesElement: this.boundary },
+            },
+          })
+        })
+      } else if (this.popper) {
+        setTimeout(() => this.popper.destroy(), 100)
+      }
+    },
+  },
+  mounted() {
+    document.addEventListener('keydown', e => {
+      if (e.keyCode === 27) {
+        this.show = false
+      }
+    })
+  },
+}
+</script>
---
+++ resources/js/Components/SearchFilter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex items-center">
+    <div class="flex w-full bg-white shadow rounded">
+      <dropdown :auto-close="false" class="px-4 md:px-6 rounded-l border-r hover:bg-gray-100 focus:border-white focus:ring focus:z-10" placement="bottom-start">
+        <div :v-slot="dropdown" class="mt-2 px-4 py-6 w-screen shadow-xl bg-white rounded" :style="{ maxWidth: `${maxWidth}px` }">
+          <slot />
+        </div>
+      </dropdown>
+    </div>
+    <button class="ml-3 text-sm text-gray-500 hover:text-gray-700 focus:text-indigo-500" type="button" @click="$emit('reset')">Reset</button>
+  </div>
+</template>
+
+<script>
+import Dropdown from '@/Components/FilterDropdown'
+
+export default {
+  components: {
+    Dropdown,
+  },
+  props: {
+    maxWidth: {
+      type: Number,
+      default: 400,
+    },
+  },
+}
+</script>
